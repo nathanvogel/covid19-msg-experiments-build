fc7aab93104a0567ca44513a3fbc29e498f108e3
Nathan Vogel <nathan@nathanvogel.com>
2020-04-01 11:46:50 +0200

    :tada:

---
+++ precache-manifest.053078f76d6811735390c93109f7d20b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4aedbfa950f9b3463116d85646eea26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f19816f8d7a354864e6",
+    "url": "/static/css/main.cbf3083f.chunk.css"
+  },
+  {
+    "revision": "5cc874937d48d0aeb823",
+    "url": "/static/js/2.eae8ac3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eae8ac3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f19816f8d7a354864e6",
+    "url": "/static/js/main.59077181.chunk.js"
+  },
+  {
+    "revision": "90b1d466762720e47afb",
+    "url": "/static/js/runtime-main.d0bda056.js"
+  }
+]);
